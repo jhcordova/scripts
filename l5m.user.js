aadbad238e564de8742c55b9f3fe2fa95772fd38
Humberto <31670415+jhcordova@users.noreply.github.com>
2022-06-14 09:10:53 -0600

    Update l5m.user.js

--- l5m.user.js
+++ l5m.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         L5 lazy migration 500000
-// @version      1.3
+// @version      1.4
 // @description  try to take over the world!
 // @author       Julio Cordova
 // @include      *
@@ -11,15 +11,15 @@
 // @exclude      *://*ari-cms.*/*
 // @exclude      *://*endeavorsuite.*/*
 // @exclude      *://*.powerbi.*/*
+// @noframes
 // ==/UserScript==
 
 
 var container = document.createElement("div");
 container.setAttribute("id", "process");
 container.innerHTML = '<div class="fixedbutton" id="btn1" style="margin-right: 200px;">Copy Specs</div><div id="btn2" class="fixedbutton">Copy Overview</div>';
+
 document.body.prepend(container);
-
-
 var styles = `.fixedbutton{position:fixed;top:0;right:0;z-index:9999;margin-right:50px;padding:10px;background-color:orange;color:#fff;cursor:pointer}.fixedbutton:hover{background-color:#000}`;
 
 var styleSheet = document.createElement("style");
@@ -33,6 +33,7 @@ button.addEventListener("click", lazy);
 
 var button2 = document.getElementById("btn2");
 button2.addEventListener("click", lazy2);
+
 
 function lazy() {
     var list = document.getElementsByClassName("cd-specs-list")[0].getElementsByTagName("li");
